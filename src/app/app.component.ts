ee7e0a44732610d89e03eb22ed49450d0700cbe5
ChristianRivero <christianriveroarnez@gmail.com>
2022-10-22 10:38:58 -0400

    added heroes editor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'tour-of-heroes';
+  title = 'Tour of Heroes';
 }
