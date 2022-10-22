0f1941a9d3b855cf2b66afa1df27801514f65c44
ChristianRivero <christianriveroarnez@gmail.com>
2022-10-22 12:22:14 -0400

    added services to fetch heroes and register app messages

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,24 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from 'src/app/Interfaces/hero';
-import { HEROES } from 'src/assets/mock-heroes';
+import { HeroService } from 'src/app/services/hero-service.service';
+import { MessageService } from 'src/app/services/message.service';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent implements OnInit {
-  heroes=HEROES;
+  heroes:Hero[]=[];
   selectedHero?: Hero;
 
-  constructor() {
+  constructor(private heroService:HeroService,private messageService: MessageService) {
     
    }
 
   ngOnInit(): void {
+    this.getHeroes();
   }
   onSelect(hero: Hero): void 
   {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  }
+  getHeroes():void
+  {
+    this.heroService.getHeroes().subscribe(heroes=>this.heroes=heroes)
   }
 
 }
